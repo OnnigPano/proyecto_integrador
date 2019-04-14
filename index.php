9987f24a59c3d10a5e998b5b363937ad1d813bb2
OnnigPano <onnigpano@gmail.com>
2019-04-14 12:52:48 -0300

    pagina de inicio a desarrollar

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/app.css">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="container">
+
+      <header class="main-header">
+
+        <nav class="user-nav">
+          <ul>
+            <li><a href="#">Iniciar Sesión</a></li>
+            <li><a href="#">Registrarse</a></li>
+            <li><a href="#"><i class="fas fa-user-alt"></i></a></li>
+            <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+          </ul>
+        </nav>
+
+          <h1 class="logo"> (LOGO Y NOMBRE)</h1>
+
+        <div class="banner">
+          <h1>DESCRIPCIÓN BREVE DE LA WEB/EMPRESA</h1>
+          <nav class="sociales">
+            <a href="#"><img src="images/facebook.png" alt="icono facebook"></a>
+            <a href="#"><img src="images/instagram.png" alt="icono instagram"></a>
+            <a href="#"><img src="images/twitter.png" alt="icono twitter"></a>
+          </nav>
+
+        </div>
+
+      </header>
+
+      <main>
+
+        <nav class="main-nav">
+          <ul>
+            <li><a href="#"><i class="fas fa-mobile-alt"></i>Celulares</a></li>
+            <li><a href="#"><i class="fas fa-laptop"></i>Notebooks</a></li>
+            <li><a href="#"><i class="fas fa-tv"></i>TVs y Monitores</a></li>
+            <li><a href="#"><i class="fas fa-tablet-alt"></i>Tablets</a></li>
+            <li><a href="#"><i class="far fa-clock"></i>Smartwatches</a></li>
+            <li><a href="#"><i class="fas fa-plus"></i>Otras Categorías</a></li>
+          </ul>
+        </nav>
+
+
+      </main>
+
+      <footer class="main-footer">
+
+        <nav class="nav-footer">
+          <ul>
+            <li><a href="#"></a><i class="fas fa-envelope"></i> Contacto</li>
+            <li><a href="#"></a><i class="fas fa-question"></i> Preguntas Frecuentes</li>
+          </ul>
+        </nav>
+
+        <p> &copy; Todos los derechos reservados</p>
+
+      </footer>
+
+    </div> <!-- Fin .container -->
+
+  </body>
+</html>
