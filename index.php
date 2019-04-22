cec24e919d259f271f1070b95be886591c96d8b4
OnnigPano <onnigpano@gmail.com>
2019-04-22 00:34:35 -0300

    fusión de las 2 maquetas

--- index.php
+++ index.php
@@ -51,6 +51,75 @@
           </ul>
         </nav>
 
+        <!-- Carousel  -->
+
+
+            <div class="row">
+              <div class="container-oferta col-12 col-lg-12 col-md-12">
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="images/summer-tech-wide.jpg" alt="First slide">
+                      <div class="carousel-caption d-md-block">
+                        <h5>SUMMER SALE</h5>
+                        <p>Descuentos de verano!</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/outlet-tech-small-wide.jpg" alt="Second slide">
+                      <div class="carousel-caption d-md-block">
+                        <h5>OUTLET</h5>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/sales-img-small-wide.jpg" alt="Third slide">
+                      <div class="carousel-caption d-md-block">
+                        <h5>SUMMER SALE</h5>
+                        <p>Descuentos de verano!</p>
+                      </div>
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+               </div>
+
+              </div>
+
+            </div>
+
+
+            <div class="row" id="container-oferta">
+              <div class="container-oferta col-12 col-lg-12 col-md-12">
+                <div class="oferta1">
+                  <h2>OFERTA 1</h2>
+                  <h3>$precio</h3>
+                  <button class="button-oferta" type="button" name="button">SHOP NOW</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="container-oferta col-12 col-lg-12 col-md-12">
+                <div class="oferta2">
+                  <h3>40% SALE</h3>
+                  <h2>OFERTA 2</h2>
+                  <button class="button-oferta" type="button" name="button">SHOP NOW</button>
+                </div>
+              </div>
+            </div>
+
 
       </main>
 
@@ -68,6 +137,10 @@
       </footer>
 
     </div> <!-- Fin .container -->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
   </body>
 </html>
