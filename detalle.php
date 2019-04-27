ec129bc450402fbbde7643e8b4b07bcd3ba3a8dd
CiroGg <ciro_@live.com.ar>
2019-04-26 23:35:36 -0300

    Detalle.php agregado

---
+++ detalle.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/app.css">
+  <title>Detalle</title>
+</head>
+  <body>
+
+
+
+    <div class="container">
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#">Iniciar Sesión</a></li>
+          <li><a href="#">Registrarse</a></li>
+          <li><a href="profile.php"><i class="fas fa-user-alt"></i></a></li>
+          <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+        </ul>
+      </nav>
+      <div class="row">
+        <div class="container-detalle">
+          <div class="container-img">
+            <img src="images/rojo-pastel.jpg" alt="Foto Del Producto">
+          </div>
+          <div class="container-datos-producto">
+            <h2>Titulo del producto</h2>
+            <h3>$9999</h3>
+            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <button class="button-oferta" type="button" name="button">Buy NOW</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
