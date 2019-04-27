a5d353b6f4ea45b8e00590dfc6f0659a3e086f5b
CiroGg <ciro_@live.com.ar>
2019-04-27 03:31:44 -0300

    Bootstrap a detalle

--- detalle.php
+++ detalle.php
@@ -19,17 +19,18 @@
           <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
         </ul>
       </nav>
-      <div class="row">
+
         <div class="container-detalle">
-          <div class="container-img">
-            <img src="images/rojo-pastel.jpg" alt="Foto Del Producto">
-          </div>
-          <div class="container-datos-producto">
-            <h2>Titulo del producto</h2>
-            <h3>$9999</h3>
-            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button class="button-oferta" type="button" name="button">Buy NOW</button>
-          </div>
+          <div class="row">
+            <div class="container-img col-lg-6 col-md-6 col-sm-6 col-12">
+              <img src="images/rojo-pastel.jpg" alt="Foto Del Producto">
+            </div>
+            <div class="container-datos-producto col-lg-6 col-md-6 col-sm-6 col-12">
+              <h2>Titulo del producto</h2>
+              <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <h3>$9999</h3>
+              <button class="button-oferta" type="button" name="button">Buy NOW</button>
+            </div>
         </div>
       </div>
     </div>
