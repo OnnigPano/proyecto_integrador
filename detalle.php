5c90b5713e12b7d5c4df87e7e699c87aff4a470d
CiroGg <ciro_@live.com.ar>
2019-04-28 20:51:55 -0300

    Mobile Friendly detalle.php

--- detalle.php
+++ detalle.php
@@ -10,11 +10,18 @@
     <div class="container-detalle">
       <div class="row">
         <div class="container-img col-lg-6 col-md-6 col-sm-6 col-12">
-          <img src="images/rojo-pastel.jpg" alt="Foto Del Producto">
+          <img src="https://http2.mlstatic.com/monitores-D_NP_592925-MLA25521012722_042017-Q.jpg" alt="Foto Del Producto">
         </div>
         <div class="container-datos-producto col-lg-6 col-md-6 col-sm-6 col-12">
           <h2>Titulo del producto</h2>
-          <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>• 1ms monitor with 144Hz refresh rate delivers a very smooth gaming experience in FPS and MOBA games</p>
+
+          <p>• Black eQualizer for visual clarity in dark scenes without overexposing bright area that help gamer to acquire targets easily and offer viewing comfort in gameplay</p>
+
+          <p>• Color Vibrance gives you the flexibility to easily tune preferred color tone and makes color grading more defined</p>
+
+          <p>• Variety of input connectors, including DisplayPort, DVI-DL, HDMI and headphone jack</p>
+
           <h3>$9999</h3>
           <button class="button-oferta" type="button" name="button">Buy NOW</button>
         </div>
