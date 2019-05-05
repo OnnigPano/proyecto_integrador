b1079ef5d53d2b5badb1a148b161d6af9c997930
hernitorres <hernan.torres90@gmail.com>
2019-05-05 20:46:04 -0300

    Se crea register-controler.php y se agrega logica de persistencia

---
+++ register-controller.php
@@ -0,0 +1,13 @@
+<?php
+//FUNCION PARA DEBAGUEAR
+function myDeBug($data){
+   echo var_dump($data);
+}
+
+//FUNCION PARA VALIDAR LOS DATOS DEL REGISTRO
+
+function registerValidate(){
+
+}
+
+ ?>
--- register.php
+++ register.php
@@ -1,8 +1,41 @@
 <?php
-
   $title= 'Registrate! - DS';
   require_once('./partials/head.php');
   require_once('./partials/header.php');
+  require_once('register-controller.php');
+
+
+//PERSISTENCIA DEL FORMULARIO
+  if ($_POST) {
+    $nameRegister= isset($_POST["nameRegister"]) ? trim($_POST["nameRegister"]) : " " ;
+    $surnameRegister= isset($_POST["surnameRegister"]) ? trim($_POST["surnameRegister"]) : " " ;
+    $nicknameRegister= isset($_POST["nicknameRegister"]) ? trim($_POST["nicknameRegister"]) : " " ;
+    $passwordRegister= isset($_POST["passwordRegister"]) ? trim($_POST["passwordRegister"]) : " " ;
+
+
+    $errors=[];
+
+    if (empty($nameRegister)) {
+      $errors["nameRegister"] = "El campo NOMBRE se encuentra vacio.";
+    }
+
+    if (empty($surnameRegister)) {
+      $errors["surnameRegister"] = "El campo APELLIDO se encuentra vacio.";
+    }
+
+    if (empty($nicknameRegister)) {
+      $errors["nicknameRegister"] = "El campo USUARIO se encuentra vacio.";
+    } //FALTA UN "ELSE" QUE VERIFIQUE QUE NO HAYA UN MISMO USUARIO EN LA BASE
+
+    if (empty($passwordRegister)) {
+      $errors["passwordRegister"] = "El campo CONTRASEÑA se encuentra vacio.";
+    } elseif (filter_var($passwordRegister,FILTER_VALIDATE_EMAIL)) {
+        $errors["passwordRegister"] = "El formato de la contraseña es invalida.";
+      }
+    }
+
+
+
 
 ?>
 
@@ -13,7 +46,7 @@
         </div>
 
         <br>
-        <form class="formRegister" action="index.php" method="post">
+        <form class="formRegister"  method="post" enctype="multipart/form-data">
 
           <div class="boxRegistry">
             <label for="">Nombre</label>
@@ -27,17 +60,17 @@
 
           <div class="boxRegistry">
             <label for="userName">Usuario</label>
-            <input type="text" name="userName" value="">
+            <input type="text" name="nicknameRegister" value="">
           </div>
 
           <div class="boxRegistry">
             <label for="">Correo electrónico</label>
-            <input type="email" name="emailUser" value="">
+            <input type="email" name="emailRegister" value="">
           </div>
 
           <div class="boxRegistry">
             <label for="">Contraseña</label>
-            <input type="password" name="userPassword" value="">
+            <input type="password" name="passwordRegister" value="">
           </div>
 
           <div class="boxRegistry">
