0e0857d4577bc4449c7adff40863b5804428df3f
CiroGg <ciro_@live.com.ar>
2019-05-08 00:45:34 -0300

    Verificacion register basica

--- register-controller.php
+++ register-controller.php
@@ -1,13 +1,67 @@
 <?php
-//FUNCION PARA DEBAGUEAR
-function myDeBug($data){
-   echo var_dump($data);
-}
 
-//FUNCION PARA VALIDAR LOS DATOS DEL REGISTRO
+
+session_start();
+
+define(ALLOWED_FORMAT_IMAGE, ['jpg','jpeg','png']);
+
 
 function registerValidate(){
 
+   $errors = [];
+
+   $nameRegister= trim($_POST['nameRegister']);
+   $surnameRegister= trim($_POST['surnameRegister']);
+   $nicknameRegister= trim($_POST['nicknameRegister']);
+   $passwordRegister= trim($_POST['passwordRegister']);
+   $emailRegister = trim($_POST['emailRegister']);
+
+   $avatarRegister = $_FILES['avatarRegister'];
+
+   if (empty($nameRegister)) {
+      $errors['nameRegister'] = 'El campo nombre es OBLIGATORIO';
+   }
+
+   if (empty($surnameRegister)) {
+      $errors['surnameRegister'] = 'El campo apellido es OBLIGATORIO';
+   }
+
+   if (empty($nicknameRegister)) {
+      $errors["nicknameRegister"] = "El campo nickname es OBLIGATORIO";
+    } //FALTA UN "ELSE" QUE VERIFIQUE QUE NO HAYA UN MISMO USUARIO EN LA BASE
+
+   if (empty($emailRegister)) {
+      $errors['emailRegister'] = 'El campo email es OBLIGATORIO';
+   }elseif (!filter_var($emailRegister, FILTER_VALIDATE_EMAIL)) {
+      $errors['emailRegister'] = 'El formato de mail no es valido';
+   } //Falta revisar si ya existe
+
+   if (empty($passwordRegister)) {
+      $errors["$passwordRegister"] = "El campo password es OBLIGATORIO";
+    } //COMPARAR CON RE-PASSWORD
+
+    if ($avatarRegister['error'] != UPLOAD_ERR_OK) {
+      $errors['$avatarRegister'] = 'Subí una imagen';
+    }else{
+      $extension = pathinfo($avatarRegister[name], PATHINFO_EXTENSION);
+
+      if (!in_array($extension,ALLOWED_FORMAT_IMAGE)) {
+        $errors[$avatarRegister] = 'Revise el formato de la imagen.';
+      }
+    }
+
+
+   return $errors;
 }
 
- ?>
+
+
+//FUNCION PARA DEBAGUEAR
+function myDeBug($data){
+      echo "<pre>";
+		var_dump($data);
+		echo "</pre>";
+		exit;
+}
+
+?>
