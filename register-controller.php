d61660f2b4fc5b1458d08493c7fd16bab36278c5
Ciro Manuel Gonzalez <ciro_@live.com.ar>
2019-05-08 21:19:21 -0300

    Validacion completa sin REpassword. Guarda usuario. Chequea mail existente

--- register-controller.php
+++ register-controller.php
@@ -3,8 +3,10 @@
 
 session_start();
 
-define(ALLOWED_FORMAT_IMAGE, ['jpg','jpeg','png']);
-define(IMAGE_FILE_PATH, dirname(__FILE__) . '/data/avatars/');
+define('ALLOWED_FORMAT_IMAGE', ['jpg','jpeg','png']);
+define('IMAGE_FILE_PATH', dirname(__FILE__) . '/data/avatars/');
+define('JSON_USERS_PATH', dirname(__FILE__) . '/data/users.json');
+
 
 
 function registerValidate(){
@@ -35,19 +37,21 @@ function registerValidate(){
       $errors['emailRegister'] = 'El campo email es OBLIGATORIO';
    }elseif (!filter_var($emailRegister, FILTER_VALIDATE_EMAIL)) {
       $errors['emailRegister'] = 'El formato de mail no es valido';
-   } //Falta revisar si ya existe
+   } elseif (checkEmailExist($emailRegister)) {
+     $errors['emailRegister'] = 'El mail ingresado ya existe en nuestra base de datos';
+   }
 
    if (empty($passwordRegister)) {
       $errors["$passwordRegister"] = "El campo password es OBLIGATORIO";
     } //COMPARAR CON RE-PASSWORD
 
     if ($avatarRegister['error'] != UPLOAD_ERR_OK) {
-      $errors['$avatarRegister'] = 'Subí una imagen';
+      $errors['avatarRegister'] = 'Subí una imagen';
     }else{
-      $extension = pathinfo($avatarRegister[name], PATHINFO_EXTENSION);
+      $extension = pathinfo($avatarRegister['name'], PATHINFO_EXTENSION);
 
       if (!in_array($extension,ALLOWED_FORMAT_IMAGE)) {
-        $errors[$avatarRegister] = 'Revise el formato de la imagen.';
+        $errors['avatarRegister'] = 'Revise el formato de la imagen.';
       }
     }
 
@@ -56,7 +60,7 @@ function registerValidate(){
 }
 
 function saveImg(){
-  $extension = pathinfo($_FILES['$avatarRegister']['name'], PATHINFO_EXTENSION);
+  $extension = pathinfo($_FILES['avatarRegister']['name'], PATHINFO_EXTENSION);
   $tempFile = $_FILES['avatarRegister']['tmp_name'];
   $finalName = uniqid( 'img_') . '.' . $extension;
   $finalPath = IMAGE_FILE_PATH . $finalName;
@@ -65,6 +69,57 @@ function saveImg(){
   return $finalName;
 }
 
+function saveUser(){
+
+  $nameRegister= trim($_POST['nameRegister']);
+  $surnameRegister= trim($_POST['surnameRegister']);
+  $nicknameRegister= trim($_POST['nicknameRegister']);
+  $emailRegister = trim($_POST['emailRegister']);
+
+  unset($_POST['passwordRegister']);
+
+  $userToSave = $_POST;
+
+  $allUsers = getAllUsers();
+  $allUsers[] = $userToSave;
+
+  file_put_contents(JSON_USERS_PATH, json_encode($allUsers));
+
+  return $userToSave;
+
+}
+
+function getAllUsers(){
+
+  $fileContent = file_get_contents(JSON_USERS_PATH);
+  $allUsers = json_decode($fileContent, true);
+  return $allUsers;
+
+}
+
+function generateId(){
+
+  $allUsers = getAllUsers();
+
+  if (count($allUsers) == 0) {
+    return 1;
+  }
+
+  $lastIdUser = array_pop($allUsers);
+  return $lastIdUser['id'] + 1;
+}
+
+function checkEmailExist($email){
+
+  $allUsers = getAllUsers();
+
+  foreach ($allUsers as $user) {
+    if ($user['emailRegister'] == $email) {
+      return true;
+    }
+  }
+return false;
+}
 
 
 //FUNCION PARA DEBAGUEAR
--- register.php
+++ register.php
@@ -1,38 +1,64 @@
 <?php
+
   $title= 'Registrate! - DS';
   require_once('./partials/head.php');
   require_once('./partials/header.php');
   require_once('register-controller.php');
 
+$errorsInRegister = [];
+
+
+//Variables persistencia
+
+$nameRegister = '';
+$surnameRegister = '';
+$nicknameRegister = '';
+$emailRegister = '';
+
+
 
 //PERSISTENCIA DEL FORMULARIO
   if ($_POST) {
+
     $nameRegister= isset($_POST["nameRegister"]) ? trim($_POST["nameRegister"]) : " " ;
     $surnameRegister= isset($_POST["surnameRegister"]) ? trim($_POST["surnameRegister"]) : " " ;
     $nicknameRegister= isset($_POST["nicknameRegister"]) ? trim($_POST["nicknameRegister"]) : " " ;
-    $passwordRegister= isset($_POST["passwordRegister"]) ? trim($_POST["passwordRegister"]) : " " ;
+    $nicknameRegister= isset($_POST["emailRegister"]) ? trim($_POST["emailRegister"]) : " " ;
+    // $passwordRegister= isset($_POST["passwordRegister"]) ? trim($_POST["passwordRegister"]) : " " ;
 
 
-    $errors=[];
+    // $errors=[];
+    //
+    // if (empty($nameRegister)) {
+    //   $errors["nameRegister"] = "El campo NOMBRE se encuentra vacio.";
+    // }
+    //
+    // if (empty($surnameRegister)) {
+    //   $errors["surnameRegister"] = "El campo APELLIDO se encuentra vacio.";
+    // }
+    //
+    // if (empty($nicknameRegister)) {
+    //   $errors["nicknameRegister"] = "El campo USUARIO se encuentra vacio.";
+    // } //FALTA UN "ELSE" QUE VERIFIQUE QUE NO HAYA UN MISMO USUARIO EN LA BASE
+    //
+    // if (empty($passwordRegister)) {
+    //   $errors["passwordRegister"] = "El campo CONTRASEÑA se encuentra vacio.";
+    // } elseif (filter_var($passwordRegister,FILTER_VALIDATE_EMAIL)) {
+    //     $errors["passwordRegister"] = "El formato de la contraseña es invalida.";
+    //   }
 
-    if (empty($nameRegister)) {
-      $errors["nameRegister"] = "El campo NOMBRE se encuentra vacio.";
+    $errorsInRegister = registerValidate();
+
+    if (!$errorsInRegister) {
+      $imgName = saveImg();
+      $_POST['avatar'] = $imgName;
+
+
+      $thisUser = saveUser();
     }
-
-    if (empty($surnameRegister)) {
-      $errors["surnameRegister"] = "El campo APELLIDO se encuentra vacio.";
-    }
-
-    if (empty($nicknameRegister)) {
-      $errors["nicknameRegister"] = "El campo USUARIO se encuentra vacio.";
-    } //FALTA UN "ELSE" QUE VERIFIQUE QUE NO HAYA UN MISMO USUARIO EN LA BASE
-
-    if (empty($passwordRegister)) {
-      $errors["passwordRegister"] = "El campo CONTRASEÑA se encuentra vacio.";
-    } elseif (filter_var($passwordRegister,FILTER_VALIDATE_EMAIL)) {
-        $errors["passwordRegister"] = "El formato de la contraseña es invalida.";
-      }
-    }
+    //myDeBug($errorsInRegister);
+      //FALTA LOGGEARLO.
+     }
 
 
 
