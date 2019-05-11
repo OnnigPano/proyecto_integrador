82ac77a31a6338e6e2589013985d79ce8885daae
OnnigPano <onnigpano@gmail.com>
2019-05-10 22:30:39 -0300

    Persistencia register, validación password sprint, validación bootstrap, pais en register

--- register-controller.php
+++ register-controller.php
@@ -13,11 +13,13 @@ function registerValidate(){
 
    $errors = [];
 
-   $nameRegister= trim($_POST['nameRegister']);
-   $surnameRegister= trim($_POST['surnameRegister']);
-   $nicknameRegister= trim($_POST['nicknameRegister']);
-   $passwordRegister= trim($_POST['passwordRegister']);
+   $nameRegister = trim($_POST['nameRegister']);
+   $surnameRegister = trim($_POST['surnameRegister']);
+   $nicknameRegister = trim($_POST['nicknameRegister']);
+   $passwordRegister = trim($_POST['passwordRegister']);
+   $repassword = trim($_POST['repassword']);
    $emailRegister = trim($_POST['emailRegister']);
+   $countryRegister = $_POST['countryRegister'];
 
    $avatarRegister = $_FILES['avatarRegister'];
 
@@ -30,30 +32,47 @@ function registerValidate(){
    }
 
    if (empty($nicknameRegister)) {
-      $errors["nicknameRegister"] = "El campo nickname es OBLIGATORIO";
+      $errors["nicknameRegister"] = "El campo usuario es OBLIGATORIO";
     } //FALTA UN "ELSE" QUE VERIFIQUE QUE NO HAYA UN MISMO USUARIO EN LA BASE
 
    if (empty($emailRegister)) {
       $errors['emailRegister'] = 'El campo email es OBLIGATORIO';
-   }elseif (!filter_var($emailRegister, FILTER_VALIDATE_EMAIL)) {
+   } elseif (!filter_var($emailRegister, FILTER_VALIDATE_EMAIL)) {
       $errors['emailRegister'] = 'El formato de mail no es valido';
    } elseif (checkEmailExist($emailRegister)) {
      $errors['emailRegister'] = 'El mail ingresado ya existe en nuestra base de datos';
    }
 
    if (empty($passwordRegister)) {
-      $errors["$passwordRegister"] = "El campo password es OBLIGATORIO";
-    } //COMPARAR CON RE-PASSWORD
+      $errors["passwordRegister"] = "El campo password es OBLIGATORIO";
+    } elseif ( strlen($passwordRegister) < 5 ) {
+      $errors["passwordRegister"] = "La contraseña debe tener más de 5 caracteres";
+    } elseif ( strpos($passwordRegister, 'DH') === false ) {
+      $errors["passwordRegister"] = "Recuerde que su contraseña debe incluir DH";
+    } elseif ( $passwordRegister != $repassword ) {
+      $errors["passwordRegister"] = "Las contraseñas ingresadas deben coincidir";
+      $errors["repassword"] = "Las contraseñas ingresadas deben coincidir";
+    } elseif ( strpos($passwordRegister, " ") !== false ) {
+      $errors['passwordRegister'] = "La contraseña no puede contener espacios";
+    }
 
-    if ($avatarRegister['error'] != UPLOAD_ERR_OK) {
+   if (empty($repassword)) {
+      $errors["repassword"] = "Debe reingresar la contraseña";
+    }
+
+    if (empty($countryRegister)) {
+      $errors['countryRegister'] = "Debe seleccionar un país";
+    }
+
+   if ($avatarRegister['error'] != UPLOAD_ERR_OK) {
       $errors['avatarRegister'] = 'Subí una imagen';
-    }else{
+   } else {
       $extension = pathinfo($avatarRegister['name'], PATHINFO_EXTENSION);
 
-      if (!in_array($extension,ALLOWED_FORMAT_IMAGE)) {
+    if (!in_array($extension,ALLOWED_FORMAT_IMAGE)) {
         $errors['avatarRegister'] = 'Revise el formato de la imagen.';
-      }
     }
+   }
 
 
    return $errors;
--- register.php
+++ register.php
@@ -7,6 +7,21 @@
 
   $errorsInRegister = [];
 
+  // Array de países para el select
+
+  $countries = [
+		'ar' => 'Argentina',
+		'bo' => 'Bolivia',
+		'br' => 'Brasil',
+		'co' => 'Colombia',
+		'cl' => 'Chile',
+		'ec' => 'Ecuador',
+		'pa' => 'Paraguay',
+		'pe' => 'Perú',
+		'uy' => 'Uruguay',
+		've' => 'Venezuela',
+	];
+
 
   //Variables persistencia
 
@@ -14,6 +29,7 @@
   $surnameRegister = '';
   $nicknameRegister = '';
   $emailRegister = '';
+  $countryFromPost = '';
 
 
 
@@ -23,7 +39,8 @@
     $nameRegister= isset($_POST["nameRegister"]) ? trim($_POST["nameRegister"]) : " " ;
     $surnameRegister= isset($_POST["surnameRegister"]) ? trim($_POST["surnameRegister"]) : " " ;
     $nicknameRegister= isset($_POST["nicknameRegister"]) ? trim($_POST["nicknameRegister"]) : " " ;
-    $nicknameRegister= isset($_POST["emailRegister"]) ? trim($_POST["emailRegister"]) : " " ;
+    $emailRegister= isset($_POST["emailRegister"]) ? trim($_POST["emailRegister"]) : " " ;
+    $countryFromPost = $_POST['countryRegister'];
 
 
     $errorsInRegister = registerValidate();
@@ -35,6 +52,7 @@
 
       $thisUser = saveUser();
     }
+
     //myDeBug($errorsInRegister);
       //FALTA LOGGEARLO.
      }
@@ -55,32 +73,109 @@
 
           <div class="boxRegistry">
             <label for="">Nombre</label>
-            <input type="text" name="nameRegister" value="">
+            <input
+              type="text"
+              class="form-control <?= isset( $errorsInRegister['nameRegister'] ) ? "is-invalid" : null ?> "
+              name="nameRegister"
+              value="<?= $nameRegister ?>"
+            >
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['nameRegister'] ) ? $errorsInRegister['nameRegister'] : null ?>
+            </div>
           </div>
 
           <div class="boxRegistry">
             <label for="">Apellido</label>
-            <input type="text" name="surnameRegister" value="">
+            <input
+              type="text"
+              class="form-control <?= isset( $errorsInRegister['surnameRegister'] ) ? "is-invalid" : null ?> "
+              name="surnameRegister"
+              value="<?= $surnameRegister ?>"
+            >
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['surnameRegister'] ) ? $errorsInRegister['surnameRegister'] : null ?>
+            </div>
           </div>
 
           <div class="boxRegistry">
             <label for="userName">Usuario</label>
-            <input type="text" name="nicknameRegister" value="">
+            <input
+              type="text"
+              class="form-control <?= isset( $errorsInRegister['nicknameRegister'] ) ? "is-invalid" : null ?> "
+              name="nicknameRegister"
+              value="<?= $nicknameRegister ?>"
+              >
+              <div class="invalid-feedback">
+                <?= isset( $errorsInRegister['nicknameRegister'] ) ? $errorsInRegister['nicknameRegister'] : null ?>
+              </div>
           </div>
 
           <div class="boxRegistry">
             <label for="">Correo electrónico</label>
-            <input type="email" name="emailRegister" value="">
+            <input
+              type="email"
+              class="form-control <?= isset( $errorsInRegister['emailRegister'] ) ? "is-invalid" : null ?> "
+              name="emailRegister"
+              value="<?= $emailRegister ?>"
+            >
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['emailRegister'] ) ? $errorsInRegister['emailRegister'] : null ?>
+            </div>
+          </div>
+
+          <div class="boxRegistry">
+            <label for="">País</label>
+            <select class="custom-select form-control <?= isset( $errorsInRegister['countryRegister'] ) ? "is-invalid" : null ?> " name="countryRegister">
+              <option value="">Elegí un país</option>
+              <?php foreach ($countries as $code => $country): ?>
+                <option
+                  value="<?= $code ?>"
+                  <?= $code == $countryFromPost ? 'selected' : null ?>
+                > <?= $country ?> </option>
+              <?php endforeach; ?>
+            </select>
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['countryRegister'] ) ? $errorsInRegister['countryRegister'] : null ?>
+            </div>
           </div>
 
           <div class="boxRegistry">
             <label for="">Contraseña</label>
-            <input type="password" name="passwordRegister" value="">
+            <input
+              type="password"
+              class="form-control <?= isset( $errorsInRegister['passwordRegister'] ) ? "is-invalid" : null ?> "
+              name="passwordRegister"
+              value=""
+            >
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['passwordRegister'] ) ? $errorsInRegister['passwordRegister'] : null ?>
+            </div>
+          </div>
+
+          <div class="boxRegistry">
+            <label for="">Repetir contraseña</label>
+            <input
+              type="password"
+              class="form-control <?= isset( $errorsInRegister['repassword'] ) ? "is-invalid" : null ?> "
+              name="repassword"
+              value=""
+              >
+              <div class="invalid-feedback">
+                <?= isset( $errorsInRegister['repassword'] ) ? $errorsInRegister['repassword'] : null ?>
+              </div>
           </div>
 
           <div class="boxRegistry">
             <label for="">Imagen</label>
-            <input class="avatarInput" type="file" name="avatarRegister" value="">
+            <input
+              class="avatarInput form-control <?= isset( $errorsInRegister['avatarRegister'] ) ? "is-invalid" : null ?> "
+              type="file"
+              name="avatarRegister"
+              value=""
+            >
+            <div class="invalid-feedback">
+              <?= isset( $errorsInRegister['avatarRegister'] ) ? $errorsInRegister['avatarRegister'] : null ?>
+            </div>
           </div>
 
           <div class="boxRegistry">
