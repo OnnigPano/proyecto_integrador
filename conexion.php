16394680f96879c5f13330cd991706b8f1f664e2
Tiagoman <tiagogonzalezgg@gmail.com>
2019-06-30 20:29:53 -0300

    Tema conexion y sigue faltando importar el json a la bd

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 
     $dsn = 'mysql:host=localhost;dbname=digital_store;port=3306';
-    $db_user= '';
+    $db_user= 'root';
     $db_pass = '';
     $opt = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
-    
+
 
     try {
         $db = new PDO($dsn, $db_user, $db_pass, $opt);
